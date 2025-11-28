b3105e4a9f3cddeefda7a0fb9b44fe062757ff36
frame007 <64419657+frame007@users.noreply.github.com>
2025-11-28 22:16:38 +0700

    Add AVR core 1.1.5

---
+++ kruframe/avr/1.1.5/post_install.bat
@@ -0,0 +1,29 @@
+@echo off
+set ARGS=/LM /SW /SA
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  drivers\dpinst-amd64.exe %ARGS%
+) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+  drivers\dpinst-amd64.exe %ARGS%
+) ELSE (
+  drivers\dpinst-x86.exe %ARGS%
+)
+
+REM dpinst /PATH has problems with relative paths, so use absolute path.
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  drivers\dpinst-amd64.exe /PATH "%cd%\drivers\gemma" %ARGS%
+) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+  drivers\dpinst-amd64.exe /PATH "%cd%\drivers\gemma" %ARGS%
+) ELSE (
+  drivers\dpinst-x86.exe /PATH "%cd%\drivers\gemma" %ARGS%
+)
+
+REM Installing Driver for Model Nano
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  drivers\CH341SER\DRVSETUP64\DRVSETUP64.exe %ARGS%
+) ELSE IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" (
+  drivers\CH341SER\DRVSETUP64\DRVSETUP64.exe %ARGS%
+) ELSE (
+  drivers\CH341SER\SETUP.exe %ARGS%
+)
+
+exit /b 0
